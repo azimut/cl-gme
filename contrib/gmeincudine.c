a9d4f26e855bdf3a2a4fbd6c6927c81313bf9efc
azimut <azimut.github@protonmail.com>
2018-07-18 04:54:16 -0300

    add contrib code

---
+++ contrib/gmeincudine.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <gme/gme.h>
+
+void gmefile_to_buffer(double *buf,
+                       Music_Emu *gmefile,
+                       unsigned long frames)
+{
+  // We read it by little chunks otherwise things blew up on sbcl
+  // probably due too much memory allocation/usage
+  short lbuf[4096];
+  unsigned long i, j;
+  for(i=0; i<frames; i++){
+    if((i % 4096) == 0){
+      gme_play(gmefile, 4096, lbuf);
+      for(j=i; j<(i + 4096); j++){
+        buf[j] = (double)lbuf[j - i];
+      }
+    }
+  }
+}
---
+++ contrib/gmeincudine.lisp
@@ -0,0 +1,16 @@
+(in-package :cl-gme)
+
+(pushnew (asdf:system-relative-pathname :cl-gme "contrib/")
+         *foreign-library-directories*
+         :test #'equal)
+
+;; (cffi:define-foreign-library gme
+;;   (:unix (:or "gmeincudine.so")))
+
+(cffi:defcfun ("gmefile_to_buffer" gmefile-to-buffer) :void
+  (buf :pointer)
+  (gmefile :pointer)
+  (frames :unsigned-long))
+
+(load-foreign-library '(:default "gmeincudine"))
+
