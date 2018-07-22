2df310f6bbfe2cfc04f5c84ed76c183c58e2c5ed
azimut <azimut.github@protonmail.com>
2018-07-22 00:00:38 -0300

    actually adding offset

--- contrib/gmeincudine.c
+++ contrib/gmeincudine.c
@@ -1,3 +1,9 @@
+// NOTE: We read it by little chunks otherwise things blew up on sbcl
+// probably due too much memory allocation/usage
+//
+// FIXME: I do need to add logic to use chunks based
+//        on the sample rate, so I can just slice by seconds
+
 #include <stdio.h>
 #include <gme/gme.h>
 
@@ -6,18 +12,21 @@ void gmefile_to_buffer(double *buf,
                        unsigned long frames,
                        unsigned long offset)
 {
-  // We read it by little chunks otherwise things blew up on sbcl
-  // probably due too much memory allocation/usage
   short lbuf[4096];
   unsigned long i, j;
-  if(offset<frames){
-    for(i=offset; i<frames; i++){
-      if((i % 4096) == 0){
-        gme_play(gmefile, 4096, lbuf);
-        for(j=i; j<(i + 4096); j++){
-          buf[j] = (double)lbuf[j - i];
-        }
+  
+  // Skip forward
+  if(offset>0){
+    gme_seek(gmefile, offset);
+  }
+  // Get these much frames
+  for(i=0; i<frames; i++){
+    if((i % 4096) == 0){
+      gme_play(gmefile, 4096, lbuf);
+      for(j=i; j<(i + 4096); j++){
+        buf[j] = (double)lbuf[j - i];
       }
     }
   }
 }
+
